b460fc4add6e292efebab9b9678e40e3607f0ee7
Taha Kaya <94247243+tahayky@users.noreply.github.com>
2025-03-26 21:38:08 +0300

    Number Abbreviation

--- Assets/Scripts/NumberAbbreviation.cs
+++ Assets/Scripts/NumberAbbreviation.cs
@@ -1,16 +1,44 @@
+using System;
+using System.Numerics;
+using TMPro;
 using UnityEngine;
 
 public class NumberAbbreviation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    TextMeshPro text;
+    public string test;
+    private void Awake()
     {
-        
+        text = GetComponent<TextMeshPro>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetNumber(string number)
     {
-        
+        _SetNumber(BigInteger.Parse(number));
+    }
+
+    private void _SetNumber(BigInteger number)
+    {
+        string[] suffixes = { "", "K", "M", "B", "T", "Q", "Qi", "Sx", "Sp", "O", "N" };
+        if (number > 9999)
+        {
+            text.text = FormatNumber(number, suffixes);
+        }
+        else
+        {
+            text.text = number.ToString();
+        }
+    }
+
+    static string FormatNumber(BigInteger number, string[] suffixes)
+    {
+        int order = 0;
+        double value = (double)number;
+        while (value >= 1000 && order < suffixes.Length - 1)
+        {
+            value /= 1000;
+            order++;
+        }
+        return $"{value:0.##}{suffixes[order]}";
     }
 }
